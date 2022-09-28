4e18510e01b4df10e0ac3058f04097baa435fbbd
Hanif <hanif.hakim@spesolution.com>
2022-09-28 13:19:46 +0700

    Create Division Model

---
+++ models/Division.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Division = sequelize.define(
+    "Division",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "divisions",
+      timestamps: true,
+    }
+  );
+
+  return Division;
+};
