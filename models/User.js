0fe08ccfe307fce25d7228921ffbc00ee48751ef
Hanif <hanif.hakim@spesolution.com>
2022-09-30 13:42:33 +0700

    Create User Model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      employee_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM,
+        values: ["ADMIN", "HR", "EMPLOYEE"],
+        allowNull: false,
+        defaultValue: "EMPLOYEE",
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "users",
+      timestamps: true,
+    }
+  );
+
+  return User;
+};
