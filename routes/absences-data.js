bdeb8d12590f4609bad349abd808c2d301f8a12a
Hanif <hanif.hakim@spesolution.com>
2022-10-06 00:10:44 +0700

    Implement in /absences routes

--- routes/absences-data.js
+++ routes/absences-data.js
@@ -2,11 +2,16 @@ var express = require("express");
 var router = express.Router();
 
 const absencesHandler = require("./handler/absences-data");
+const verifyRole = require("../middlewares/verifyRole");
 
 router.post("/clock-in", absencesHandler.clockIn);
 router.put("/clock-out", absencesHandler.clockOut);
 router.get("/my-absences", absencesHandler.getMyAbsences);
-router.get("/", absencesHandler.getAll);
-router.get("/:id", absencesHandler.getAbsencesByUserId);
+router.get("/", verifyRole("ADMIN", "HR"), absencesHandler.getAll);
+router.get(
+  "/:id",
+  verifyRole("ADMIN", "HR"),
+  absencesHandler.getAbsencesByUserId
+);
 
 module.exports = router;
