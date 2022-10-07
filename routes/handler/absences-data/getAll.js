43b3d9a2dd50f4ec38158c932a8e496de857ff5d
Hanif <hanif.hakim@spesolution.com>
2022-10-07 13:20:14 +0700

    Implement pagination in get all absences data

--- routes/handler/absences-data/getAll.js
+++ routes/handler/absences-data/getAll.js
@@ -1,9 +1,12 @@
 const { AbsenceData, Setting } = require("../../../models");
 const SQL = require("sequelize");
+const Pagination = require("../../../helpers/Pagination");
 
 module.exports = async (req, res) => {
   month = req.query.month ?? new Date().getMonth() + 1;
   year = req.query.year ?? new Date().getFullYear();
+  const { page, size } = req.query;
+  const { limit, offset } = Pagination.getPagination(page, size);
   const attributes = [
     "date",
     "clock_in",
@@ -32,7 +35,7 @@ module.exports = async (req, res) => {
     },
   ];
 
-  const absencesData = await AbsenceData.findAndCountAll({
+  const data = await AbsenceData.findAndCountAll({
     attributes,
     include,
     where: {
@@ -40,7 +43,10 @@ module.exports = async (req, res) => {
       [SQL.Op.and]: SQL.where(SQL.fn("YEAR", SQL.col("date")), parseInt(year)),
     },
     order: [["date", "DESC"]],
+    limit,
+    offset,
   });
+  const absencesData = Pagination.getPagingData(data, page, limit);
 
   return res.json({
     status: "success",
