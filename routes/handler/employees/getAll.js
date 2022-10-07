9766ca5c311989c88b3181659a7bcfcc0699d37c
Hanif <hanif.hakim@spesolution.com>
2022-10-07 14:52:06 +0700

    implement pagination in get all employees

--- routes/handler/employees/getAll.js
+++ routes/handler/employees/getAll.js
@@ -1,7 +1,10 @@
 const Sqlize = require("sequelize");
 const { Employee } = require("../../../models");
+const Pagination = require("../../../helpers/Pagination");
 
 module.exports = async (req, res) => {
+  const { page, size } = req.query;
+  const { limit, offset } = Pagination.getPagination(page, size);
   const name = req.query.name;
   let condition = name ? { name: { [Sqlize.Op.like]: `%${name}%` } } : null;
   const attributes = ["id", "name", "date_of_birth", "email", "phone_number"];
@@ -18,11 +21,15 @@ module.exports = async (req, res) => {
     },
   ];
 
-  const employees = await Employee.findAll({
+  const data = await Employee.findAndCountAll({
     attributes: attributes,
     include: include,
     where: condition,
+    limit,
+    offset,
   });
+
+  const employees = Pagination.getPagingData(data, page, limit);
 
   return res.json({
     status: "success",
